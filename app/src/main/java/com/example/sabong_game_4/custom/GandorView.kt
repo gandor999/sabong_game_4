847c107d5daeac7fc28d1261ace3f56b8a83b5c2
Geodor Ruales <gandor999@gmail.com>
2025-01-28 15:59:42 +0800

    chore: delete some comments and logs

--- app/src/main/java/com/example/sabong_game_4/custom/GandorView.kt
+++ app/src/main/java/com/example/sabong_game_4/custom/GandorView.kt
@@ -2,22 +2,11 @@ package com.example.sabong_game_4.custom
 
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
-import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 
 class GandorView(context: Context) : View(context), ICustomView {
     override fun setViewDimensions(width: Int, height: Int) {
-        Log.d(
-            "Gandor",
-            "resources.displayMetrics.widthPixels: ${resources.displayMetrics.widthPixels}"
-        )
-        Log.d(
-            "Gandor",
-            "resources.displayMetrics.heightPixels: ${resources.displayMetrics.heightPixels}"
-        )
-
-        // add ratio conversion
         // was developing the dimensions on a Pixel 3a XL API 34
         // ratio is (150 / 2160) = (x / widthPixels) => x = (150 * widthPixels / 2160)
         layoutParams = LinearLayout.LayoutParams(
